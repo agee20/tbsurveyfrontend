9c29e50bfd674af5e244ee87ab88dfcbfa74ee0d
mjd165 <mjd165@uakron.edu>
2024-04-11 18:55:07 -0400

    Good version Results Page

--- resultsController.js
+++ resultsController.js
@@ -17,6 +17,19 @@ window.addEventListener('resize', centerThankYou);
 // Call the centerThankYou function initially to center on page load
 centerThankYou();
 
+
+// Update result message based on survey result
+const resultMessageElement = document.getElementById('result-message');
+const surveyResult = ''; // replace this with the actual survey result
+
+if (surveyResult === 'positive') {
+  resultMessageElement.innerHTML = '<p>Your TB screening result is <span style="color: red;">positive</span>.</p>';
+} else if (surveyResult === 'negative') {
+  resultMessageElement.innerHTML = '<p>Your TB screening result is <span style="color: green;">negative</span>.</p>';
+} else {
+  resultMessageElement.innerHTML = '<p>Your TB screening result is pending. Check your email for further instructions.</p>';
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     // Get the button element
     const loginButton = document.querySelector('.button-blue');
